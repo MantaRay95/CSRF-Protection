9c8dd954fe609c2c5e742eb5ef94f596de618c7b
Pavithra <pavithrarasanjana61@gmail.com>
2018-05-09 17:39:18 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title> CSRF protection</title>
+		<link rel="stylesheet" href="public/css/bootstrap.min.css">
+        <script src="public/js/jquery.min.js"></script>
+        <script src="public/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<button type="submit" class="btn btn-success btn-block" id="dsc" name="dsc" >CSRF protection by Syncronize Token pattern </button>
+		<button type="submit" class="btn btn-info btn-block" id="dsc" name="dsc" >CSRF protection by double submit cookies </button>
+	</body>
+</html>
